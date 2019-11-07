b5bbfef51e7ba7b270f819c7a2bf6cdca51dd115
Alexis Cataldi <acataldi@emi.u-bordeaux1.fr>
2019-11-07 16:04:36 +0100

    début ACI

---
+++ Td4.5/CalculACI.m
@@ -0,0 +1,6 @@
+function [ W ] = CalculACI( Smpl1 , Smpl2 )
+%CALCULACI Summary of this function goes here
+%   Detailed explanation goes here
+% W = Inv(Sum(V(Smpl1)+V(Smpl2)))*transpose(mu1-mu2) Pour 2 class
+end
+
